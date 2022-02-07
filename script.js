84860091535cc9196f6769b3c22e5e1545f17043
avagagner <46656654+avagagner@users.noreply.github.com>
2022-02-07 17:04:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,74 @@
-var mymap = L.map('mapid').setView([38.63775922635261, -90.28368046396037], 13);
-//https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
-//'http://a.tile.stamen.com/toner/{z}/{x}/{y}.png'
-//https://wiki.openstreetmap.org/wiki/Tile_servers
-L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 1,
-	maxZoom: 16,
-	ext: 'jpg'
-}).addTo(mymap);
 
-var marker = L.marker([38.63672876590212, -90.24674730564345]).addTo(mymap);
+      require(["esri/Map", "esri/layers/CSVLayer", "esri/views/MapView", "esri/widgets/Legend"], (
+        Map,
+        CSVLayer,
+        MapView,
+        Legend
+      ) => {
+        const url = "https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%202%20-%20ArcGIS%20JavaScript%20API/stl_crime_wgs_84.csv";
 
-var marker2 = L.marker([38.63816176328897, -90.25104626536422]).addTo(mymap);
+        // Paste the url into a browser's address bar to download and view the attributes
+        // in the CSV file. These attributes include:
+        // * mag - magnitude
+        // * type - earthquake or other event such as nuclear test
+        // * place - location of the event
+        // * time - the time of the event
 
-var marker3 = L.marker([38.661539699331904, -90.30894447950988]).addTo(mymap);
+        const template = {
+          title: "Crime committed at {ILEADSStreet}",
+        };
 
-var marker4 = L.marker([38.61714841986633, -90.27511168787689]).addTo(mymap);
+        // The heatmap renderer assigns each pixel in the view with
+        // an intensity value. The ratio of that intensity value
+        // to the maxPixel intensity is used to assign a color
+        // from the continuous color ramp in the colorStops property
 
-var marker5 = L.marker([38.64751484426265, -90.2744901289432]).addTo(mymap);
+        const renderer = {
+          type: "heatmap",
+          colorStops: [
+            { color: "rgba(63, 40, 102, 0)", ratio: 0 },
+            { color: "#001219", ratio: 0.083 },
+            { color: "#252433", ratio: 0.166 },
+            { color: "#005F73", ratio: 0.249 },
+            { color: "#0A9396", ratio: 0.332 },
+            { color: "#94D2BD", ratio: 0.415 },
+            { color: "#E9D8A6", ratio: 0.498 },
+            { color: "#EE9B00", ratio: 0.581 },
+            { color: "#CA6702", ratio: 0.664 },
+            { color: "#BB3E03", ratio: 0.747 },
+            { color: "#AE2012", ratio: 0.83 },
+            { color: "#9B2226", ratio: 0.913 },
+            { color: "#820000", ratio: 1 }
+          ],
+          maxPixelIntensity: 25,
+          minPixelIntensity: 0
+        };
 
+        const layer = new CSVLayer({
+          url: url,
+          title: "St. Louis Crime Heatmap",
+          copyright: "St. Louis Police Department",
+          popupTemplate: template,
+          renderer: renderer
+        });
 
+        const map = new Map({
+          basemap: "gray-vector",
+          layers: [layer]
+        });
 
-marker.bindPopup("<b>Scottish Arms</b>.").openPopup();
-marker2.bindPopup("<b>Scarlett's Wine Bar</b>.").openPopup();
-marker3.bindPopup("<b>Mi Ranchito</b>.").openPopup();
-marker4.bindPopup("<b>Lorenzo's Trattoria</b>.").openPopup();
-marker4.bindPopup("<b>2Shae Cafe</b>.").openPopup();
+        const view = new MapView({
+          container: "viewDiv",
+          center: [-90.194, 38.6270],
+          zoom: 13,
+          map: map
+        });
+
+        view.ui.add(
+          new Legend({
+            view: view
+          }),
+          "bottom-left"
+        );
+      });
+   
